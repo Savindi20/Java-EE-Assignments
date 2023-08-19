53599fbcd7e871f64c433446ac7659109bd210c2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-08-19 16:18:13 +0530

    get all method exception alert added

--- Json-with-Ajax/src/servlet/CustomerServlet.java
+++ Json-with-Ajax/src/servlet/CustomerServlet.java
@@ -32,12 +32,21 @@ public class CustomerServlet extends HttpServlet {
                 customer.add("salary", rst.getDouble("salary"));
                 allCustomers.add(customer.build());
             }
-
             resp.addHeader("Content-Type","application/json");
-            resp.getWriter().print(allCustomers.build());
 
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            JsonObjectBuilder job = Json.createObjectBuilder();
+            job.add("state","OK");
+            job.add("message","Successfully Loaded..!");
+            job.add("data",allCustomers.build());
+            resp.getWriter().print(job.build());
+
+        }catch (ClassNotFoundException | SQLException e){
+            JsonObjectBuilder rjo = Json.createObjectBuilder();
+            rjo.add("state","Error");
+            rjo.add("message",e.getLocalizedMessage());
+            rjo.add("data","");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print(rjo.build());
         }
     }
 
